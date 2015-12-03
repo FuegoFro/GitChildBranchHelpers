f3ba7d682d26bd1eaebcfb1531c986ba697415bb
Danny Weinberg <danny@dropbox.com>
2015-12-03 10:17:40 -0800

    Forward child arc diff options to the arc command.

--- src/arc_diff_against_child.py
+++ src/arc_diff_against_child.py
@@ -1,15 +1,20 @@
-import argparse
+import sys
+
 from git_helpers import get_current_branch, arc
 from git_rebase_children import get_branch_tracker
 
 
-def main():
+def main(extra_arc_diff_options):
+    if extra_arc_diff_options:
+        extra_args = " " + " ".join(extra_arc_diff_options)
+    else:
+        extra_args = ""
+
     current_branch = get_current_branch()
     with get_branch_tracker() as tracker:
         parent = tracker.parent_for_child(current_branch)
-        arc("diff %s" % parent)
+        arc("diff %s%s" % (parent, extra_args))
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    args = parser.parse_args()
-    main()
+    extra_arc_diff_options = sys.argv[1:]
+    main(extra_arc_diff_options)
